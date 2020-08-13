184cc408edc8b9f6cf6fa291b9c4d3118a2ff062
Mike <mikegeorgetvm@gmail.com>
2020-08-13 13:27:59 +0530

    Version 02

--- demoproj01/src/main/java/com/example/demo/DemoApplication.java
+++ demoproj01/src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ package com.example.demo;
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s! from Version 01", name);
+		return String.format("Hello %s! from Version 02", name);
 	}
 }
