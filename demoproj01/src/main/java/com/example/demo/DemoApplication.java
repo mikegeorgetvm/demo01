0eb1903f695110eb2f0630f35c1fbf1583990f50
Mia <miageorgetvm@gmail.com>
2020-08-13 16:24:52 +0530

    Mia Change 01

--- demoproj01/src/main/java/com/example/demo/DemoApplication.java
+++ demoproj01/src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ package com.example.demo;
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s! from Version 02", name);
+		return String.format("Hello %s! from Mia Version 01", name);
 	}
 }
